10fe8f9d2fc024aab0bec68456a62c27abe4c31d
Adejorin Ayomiposi <adejorinayomiposi@gmail.com>
2025-11-09 22:46:29 +0100

    third commit

--- routes/ping.js
+++ routes/ping.js
@@ -1,16 +1,8 @@
 import express from "express";
-import cors from "cors";
+const router = express.Router();
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// Health check route
-app.get("/ping", (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json({ message: "pong" });
 });
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default router;
