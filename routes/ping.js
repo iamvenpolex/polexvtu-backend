571c61eeb366ee53c60dcdab9c91da24ab7d6b1a
Adejorin Ayomiposi <adejorinayomiposi@gmail.com>
2025-11-09 22:52:42 +0100

    third commit

--- routes/ping.js
+++ routes/ping.js
@@ -1,8 +1,8 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.status(200).json({ message: "pong" });
 });
 
-export default router;
+module.exports = router;
